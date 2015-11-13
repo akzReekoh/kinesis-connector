d41a6bb8c56f3eb8525c945e9ae50b082a059974
akzReekoh <achilles.dinglasan@p3ople4u.com>
2015-11-13 14:33:55 +0800

    REEKOH-504
    -Finshed sending of data to AWS Kinesis stream
    -Finished documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# kinesis-connector
+# AWS Kinesis Connector
+[![Build Status](https://travis-ci.org/Reekoh/kinesis-connector.svg)](https://travis-ci.org/Reekoh/kinesis-connector)
+![Dependencies](https://img.shields.io/david/Reekoh/kinesis-connector.svg)
+![Dependencies](https://img.shields.io/david/dev/Reekoh/kinesis-connector.svg)
+![Built With](https://img.shields.io/badge/built%20with-gulp-red.svg)
+
 AWS Kinesis Connector Plugin for the Reekoh IoT Platform. Integrates a Reekoh instance with AWS Kinesis Streams to push and synchronise device data to the AWS Pipeline.
+
+## Description
+This plugin saves all data from connected devices to the Reekoh Instance to an AWS Kinesis Stream.
+
+## Configuration
+To configure this plugin, an Amazon AWS Kinesis account and Kinesis stream is needed to provide the following:
+
+1. Access Key ID - AWS Access Key ID to use.
+2. Secret Access Key - AWS Secret Access Key to use.
+3. Region - AWS Region to use.
+4. API Version - AWS API Version to use.
+5. Stream Name - AWS Kinesis Stream Name to use.
+6. Partition Key - Corresponding AWS Kinesis Stream Partition Key to use (this is the shard ID).
+
+These parameters are then injected to the plugin from the platform.
---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var platform = require('./platform'),
+	config;
+
+/*
+ * Listen for the data event.
+ */
+platform.on('data', function (data) {
+    var AWS = require('aws-sdk'),
+        kinesis = new AWS.Kinesis({
+            accessKeyId: config.accessKeyId,
+            secretAccessKey: config.secretAccessKey,
+            region: config.region,
+            version: config.apiVersion,
+            sslEnabled: true
+        }),
+        params = {
+            Data: JSON.stringify(data),
+            PartitionKey: config.PartitionKey,
+            StreamName: config.StreamName
+        };
+
+    kinesis.putRecord(params, function(error, response){
+        if(error) platform.handleException(error);
+        else{
+            platform.log(JSON.stringify({
+                title: 'Kinesis record saved.',
+                data: params
+            }));
+        }
+    });
+});
+
+/*
+ * Event to listen to in order to gracefully release all resources bound to this service.
+ */
+platform.on('close', function () {
+    platform.notifyClose();
+});
+
+/*
+ * Listen for the ready event.
+ */
+platform.once('ready', function (options) {
+    config = {
+        accessKeyId : options.access_key_id,
+        secretAccessKey : options.secret_access_key,
+        region : options.region,
+        version : options.api_version,
+        PartitionKey: options.partition_key,
+        StreamName: options.stream_name
+    };
+
+    platform.log('Kinesis Connector Initialized.');
+	platform.notifyReady();
+});
