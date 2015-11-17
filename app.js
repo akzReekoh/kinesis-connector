083173356fc2c85432d216985c4d75993dbbd4be
Benj Sicam <bsicam@reekoh.com>
2015-11-17 23:08:17 +0800

    Performance Optimizations

--- app.js
+++ app.js
@@ -1,58 +1,65 @@
 'use strict';
 
-var platform = require('./platform'),
-	config;
+var _        = require('lodash'),
+	AWS      = require('aws-sdk'),
+	platform = require('./platform'),
+	config, kinesis;
 
 /*
  * Listen for the data event.
  */
 platform.on('data', function (data) {
-    var AWS = require('aws-sdk'),
-        kinesis = new AWS.Kinesis({
-            accessKeyId: config.accessKeyId,
-            secretAccessKey: config.secretAccessKey,
-            region: config.region,
-            version: config.apiVersion,
-            sslEnabled: true
-        }),
-        params = {
-            Data: JSON.stringify(data),
-            PartitionKey: config.PartitionKey,
-            StreamName: config.StreamName
-        };
+	if (_.isPlainObject(data)) {
+		var params = {
+			Data: JSON.stringify(data),
+			PartitionKey: config.PartitionKey,
+			StreamName: config.StreamName
+		};
 
-    kinesis.putRecord(params, function(error, response){
-        if(error) platform.handleException(error);
-        else{
-            platform.log(JSON.stringify({
-                title: 'Kinesis record saved.',
-                data: params
-            }));
-        }
-    });
+		kinesis.putRecord(params, function (error) {
+			if (error)
+				platform.handleException(error);
+			else {
+				platform.log(JSON.stringify({
+					title: 'Kinesis record saved.',
+					data: {
+						Data: data,
+						PartitionKey: config.PartitionKey,
+						StreamName: config.StreamName
+					}
+				}));
+			}
+		});
+	}
+	else
+		platform.handleException(new Error('Invalid data received.'));
 });
 
 /*
  * Event to listen to in order to gracefully release all resources bound to this service.
  */
 platform.on('close', function () {
-    platform.notifyClose();
+	platform.notifyClose();
 });
 
 /*
  * Listen for the ready event.
  */
 platform.once('ready', function (options) {
-    //set config params
-    config = {
-        accessKeyId : options.access_key_id,
-        secretAccessKey : options.secret_access_key,
-        region : options.region,
-        version : options.api_version,
-        PartitionKey: options.partition_key,
-        StreamName: options.stream_name
-    };
+	//set config params
+	config = {
+		PartitionKey: options.partition_key,
+		StreamName: options.stream_name
+	};
 
-    platform.log('Kinesis Connector Initialized.');
+	kinesis = new AWS.Kinesis({
+		accessKeyId: options.access_key_id,
+		secretAccessKey: options.secret_access_key,
+		region: options.region,
+		version: options.api_version,
+		sslEnabled: true
+	});
+
+	platform.log('Kinesis Connector Initialized.');
 	platform.notifyReady();
 });
